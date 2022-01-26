253ebfa26c37fee054a52105c7d890eb00e07564
sheng <shengwangandy@foxmail.com>
2022-01-26 21:14:50 +0800

    add dll copy for cc.exe

---
+++ copyDll.bat
@@ -0,0 +1,8 @@
+copy /y ..\..\..\libs\CCAppCommon\Release\CCAppCommon.dll .\\
+copy /y  ..\..\..\libs\qCC_glWindow\Release\QCC_GL_LIB.dll .\\
+copy /y ..\..\..\libs\CCPluginAPI\Release\CCPluginAPI.dll .\\
+copy /y ..\..\..\libs\qCC_io\Release\QCC_IO_LIB.dll .\\
+copy /y ..\..\..\libs\qCC_db\Release\QCC_DB_LIB.dll .\\
+copy /y ..\..\..\libs\qCC_db\extern\CCCoreLib\Release\CCCoreLib.dll .\\
+copy /y ..\..\..\libs\CCFbo\Release\CC_FBO_LIB.dll .\\
+
